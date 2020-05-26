236caddc676b982cc371c2e956a3e003dd3d0919
leonard percival <leonardpercival@gmail.com>
2020-05-26 14:04:08 +0100

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login' }
   get :contact, to: 'pages#contact'
   post :contact, to: 'pages#mail_contact'
+  get :services, to: 'pages#services'
   resources :projects, only: %i[index show]
   namespace :admin do
     authenticate :user, ->(user) { user.admin? } do

8bcf9f907d9fa5447dcc1c64f41b8c2855c747e3
leonard percival <leonardpercival@gmail.com>
2020-07-16 13:47:14 +0100

    done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
-    @projects = Project.includes(:services).all
+    @projects = Project.includes(:services).all.shuffle
   end
 
   def show

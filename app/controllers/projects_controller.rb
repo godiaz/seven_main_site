94d0d32dab94693e168ee3a9d6e4421630060874
leonard percival <leonardpercival@gmail.com>
2020-07-21 17:17:44 +0100

    unpublish toworknow

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,12 @@ class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
-    @projects = Project.includes(:services).all.shuffle
+    @projects = Project.includes(:services).where(published: true).shuffle
   end
 
   def show
     @project = Project.friendly.find(params[:id])
-    @projects = Project.includes(:services).all
+    @projects = Project.includes(:services).where(published: true) - @project
   end
 
 end
